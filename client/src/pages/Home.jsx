fda9b751db7bd5f96448afb3fbc20723f981fca8
Micheal PallipaRambil <114773646+MikeRambil@users.noreply.github.com>
2023-06-14 23:52:40 -0500

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dalle-clone-nxb4.onrender.com', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
